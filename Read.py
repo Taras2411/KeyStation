03f29778a00db73fa691a82d62e2a5eec1b8da13
Pushkar Taras <taraspushkar24@gmail.com>
2021-10-02 17:48:38 +0300

    multitasking test. Its alive

--- Read.py
+++ Read.py
@@ -1,12 +1,28 @@
+from threading import Thread
+from time import sleep
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
-
-try:
+def rfidScaner():
+    while True:
         id, text = reader.read()
         print(id)
-        print(text)
-finally:
-        GPIO.cleanup()
+def testSecondFunction():
+    while True:
+        for i in range(255):
+            print(f"out from test func {i}")
+            sleep(0.5)
+        
+rfidThread = Thread(target=rfidScaner)
+backThread = Thread(target=testSecondFunction)
+rfidThread.start()
+backThread.start()
+
+print("app stop")
+
+
+
+            
+
