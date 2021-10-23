b6e1cac12d37c54958c92567c6f0a2c9f9217e47
Pushkar Taras <taraspushkar24@gmail.com>
2021-10-23 16:31:29 +0300

    Pass to DB is now in environment variable

--- MYSQLTEST.py
+++ MYSQLTEST.py
@@ -1,17 +1,18 @@
 from getpass import getpass
 import mysql.connector 
+import os
 
 mydb = mysql.connector.connect(
-    host="103.167.136.18",
-    port='11004',
-    user="root",
-    password="OHAxrk22735",
-    database="key_statyion")
+    host="localhost",
+    port='3306',
+    user="admin",
+    password=os.environ['DBPASSWORD'],
+    database="key_station")
     
         
         
 mycursor = mydb.cursor()
-sql = "SELECT Card, en FROM key_statyion.Cards JOIN key_statyion.Names_cards ON key_statyion.Cards.id = key_statyion.Names_cards.Card_id JOIN key_statyion.Names ON key_statyion.Names_cards.Name_id = key_statyion.Names.id WHERE name IN ('Ivan')"
+sql = "SELECT Card, en FROM key_station.Cards JOIN key_station.Names_cards ON key_station.Cards.id = key_station.Names_cards.Card_id JOIN key_station.Names ON key_station.Names_cards.Name_id = key_station.Names.id WHERE name IN ('Ivan')"
         
 mycursor.execute(sql)
 
---
+++ environ.py
@@ -0,0 +1,8 @@
+import os
+ 
+# printing environment variables
+#print(os.environ)
+
+#for name, value in os.environ.items():
+#    print(f'{name} = {value}')
+print(os.environ['DBPASSWORD'])   
