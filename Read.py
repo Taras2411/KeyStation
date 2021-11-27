c0a1b5e3abae04d6664b7f41c96862c6a942e996
Pushkar Taras <taraspushkar24@gmail.com>
2021-11-27 18:03:08 +0200

    Cut all #

--- Read.py
+++ Read.py
@@ -1,20 +1,3 @@
-#BiperPin = 26
-#timeToGetKey = 15
-
-
-# rooms_to_pins = {
-#     '44': 21,
-#     '45': 20,
-#     '46': 16
-#     }
-# rooms_to_leds = {
-#     '44':0,
-#     '45':1,
-#     '46':2
-#     }
-
-
-
 from threading import Thread
 from time import sleep
 import mysql.connector 
@@ -26,15 +9,6 @@ import configparser
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-trueStartTime = time.time()
-pixel_pin = board.D18
-num_pixels = 3
-ORDER = neopixel.GRB
-pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
-)
-
-
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -43,6 +17,20 @@ for section in config.sections():
     dictionary[section] = {}
     for option in config.options(section):
         dictionary[section][option] = config.get(section, option)
+
+
+
+trueStartTime = time.time()
+pixel_pin = board.D18
+num_pixels = int(dictionary['OTHER']['numpixels'])
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+
+
+
 
 testDict = dictionary['ROOMS_TO_PINS']
 for i in testDict:   
@@ -149,7 +137,7 @@ def rfidScaner():
     while True:
         
         id, text = reader.read()
-        #pixels.fill((0,0,0))
+        
         global LastCardCode
         global LastDetectTime
         LastCardCode = id
@@ -157,19 +145,6 @@ def rfidScaner():
         
         print("DETECTED")
         bipOnce()
-        #print(id)
-        #temp = str(id)
-        #roomsAndEnGet = f"SELECT Name,Teachers.en,Cards.en,FIO FROM key_station.Keys JOIN key_station.TeachersToKeys ON key_station.Keys.id = key_station.TeachersToKeys.KeyId JOIN key_station.Teachers ON key_station.Teachers.id = key_station.TeachersToKeys.TeacherId JOIN key_station.Cards ON key_station.Cards.TeacherId = key_station.Teachers.id WHERE Card IN ('{temp}')"
-        #mycursor.execute(roomsAndEnGet)
-        #restuple = mycursor.fetchall()
-        #for i in restuple:
-        #   roomnum = i[0]
-        #    rooms_to_leds[roomnum]
-        #    pixels[rooms_to_leds[roomnum]]=((0, 255, 0))
-        #sleep(timeToGetKey)
-        #pixels.fill((0,0,0))
-        #bipTwice()
-        
         
 def testSecondFunction():
         print(readMagState(rooms_to_pins))
@@ -193,7 +168,7 @@ def WinDef():
         
         ###LED IND START
         
-        #pixels.fill((0,0,0))
+  
         clearAllNonRedManual()
         if TimeFromStart()- LastDetectTime < timeToGetKey:
             
@@ -215,8 +190,7 @@ def WinDef():
             
             clearAllNonRed()
             
-            #pixels.fill((0,0,0))
-            #pixels.show()
+            
         
         
     ###LED IND END
@@ -268,7 +242,7 @@ def WinDef():
                     
                 else:
                     pixels.show()
-                    #bipThrice()
+                    
                     
                     global alarm
                     alarm = True
@@ -308,30 +282,7 @@ def WinDef():
     print("FUNC STOP")
     
     
-def LedIndication():
-    pixels.fill((0,0,0))
-    pixels.show()
-    while True:
-        pixels.fill((0,0,0))
-        if TimeFromStart()- LastDetectTime < timeToGetKey:
-            
-            temp = str(LastCardCode)
-            roomsAndEnGet = f"SELECT Name,Teachers.en,Cards.en,FIO FROM key_station.Keys JOIN key_station.TeachersToKeys ON key_station.Keys.id = key_station.TeachersToKeys.KeyId JOIN key_station.Teachers ON key_station.Teachers.id = key_station.TeachersToKeys.TeacherId JOIN key_station.Cards ON key_station.Cards.TeacherId = key_station.Teachers.id WHERE Card IN ('{temp}')"
-            mycursor.execute(roomsAndEnGet)
-            restuple = mycursor.fetchall()
-            for i in rooms_to_leds:
-               for z in restuple:
-                   if i in z:
-                       pixels[rooms_to_leds[i]]=((0, 255, 0))
-                   #else:
-                       #pixels[rooms_to_leds[i]]=((0, 0, 0))
-            pixels.show()
-            num = 0
 
-        else:
-            pixels.fill((0,0,0))
-            pixels.show()
-    
     
     
 def TimeFromStart():
@@ -354,7 +305,7 @@ def Alarmer():
 WinDefThread = Thread(target=WinDef)
 WinDefThread.start()
 rfidThread.start()
-LedIndicationThread = Thread(target=LedIndication)
+#LedIndicationThread = Thread(target=LedIndication)
 AlarmerThread = Thread(target=Alarmer)
 AlarmerThread.start()
 
